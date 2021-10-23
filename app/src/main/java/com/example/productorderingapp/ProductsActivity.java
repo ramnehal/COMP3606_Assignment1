b05d6435d271eeafac302fbe96f25cc7305ccc6e
NARIN <narinramnehal21@gmail.com>
2021-10-23 02:27:09 -0400

    test

---
+++ app/src/main/java/com/example/productorderingapp/ProductsActivity.java
@@ -0,0 +1,26 @@
+package com.example.productorderingapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
